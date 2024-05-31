c03b25e5109a9fca558b222ca7744db2e3702e64
Bekbolot <justworking8888@gmail.com>
2024-05-31 17:23:24 +0600

    fsdfsd

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+function Main() {
+    return (
+        <section className='main'>
+            flasjhfsdajnf
+            dsaf
+            asd
+            flasjhfsdajnfdsaf
+            sad
+            flasjhfsdajnfdsafdsaf
+
+        </section>
+    )
+}
+
+export default Main
