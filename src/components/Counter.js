4415614e73b77bfb67f6be0693efb964d07f1921
abdulbasir <nigorakhadieva0@gmail.com>
2024-06-01 16:53:53 +0600

    d

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,27 @@
-import { useState } from "react"
+import React, { useState } from 'react';
 
 function Counter() {
-    const [state, setState] = useState(1)
-
+    const [state, setState] = useState(1);
 
     function add() {
-        setState(state + 1)
+        if (state < 50) {
+            setState(state + 1);
+        }
     }
+
     function remove() {
-        setState(state - 1)
+        if (state > 0) {
+            setState(state - 1);
+        }
     }
+
     return (
         <div>
             <h1>{state}</h1>
             <button onClick={add}>+</button>
             <button onClick={remove}>-</button>
         </div>
-    )
+    );
 }
 
-export default Counter
+export default Counter;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,85 @@
-function Main() {
-    return (
-        <section className='main'>
-            flasjhfsdajnf
-            dsaf
-            asd
-            flasjhfsdajnfdsaf
-            sad
-            flasjhfsdajnfdsafdsaf
+// import { useState } from 'react'
+// import data from '../data/db.json'
+// import Card from './Card'
 
-        </section>
-    )
+// function Main() {
+//     const [users, setUsers] = useState(data)
+//     function AddNewObg() {
+//         setUsers([...users, {
+            
+//                 "title": "bbbbbbbbbbb",
+//                 "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/954.jpg",
+//                 "despretion": "bbbbbbbbbbbbbb",
+//                 "name": "bbbbbbbbbbbbb",
+//                 "rate": 58,
+//                 "id": "1"
+                
+//         },])
+        
+//     }
+
+//     return (
+//         <section className='main'>
+//             {
+//                 users.map((item) => 
+//                     <Card 
+//                         title={item.title}
+//                         avatar={item.avatar}
+//                         despretion={item.despretion}
+//                         name={item.name}
+//                     />)
+//             }
+//                     <button onClick={AddNewObg}>add</button>
+//         </section>
+//     )
+// }
+
+// export default Main
+
+import React, { useState } from 'react';
+import data from '../data/db.json';
+import Card from './Card';
+import Sidebar from './Sidebar';
+import './Main.css'; // Добавьте CSS файл для стилизации
+
+function Main() {
+    const [users, setUsers] = useState(data);
+    const [sidebarVisible, setSidebarVisible] = useState(false);
+
+    function AddNewObg() {
+        setUsers([...users, {
+            "title": "bbbbbbbbbbb",
+            "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/954.jpg",
+            "despretion": "bbbbbbbbbbbbbb",
+            "name": "bbbbbbbbbbbbb",
+            "rate": 58,
+            "id": "1"
+        }]);
+    }
+
+    function toggleSidebar() {
+        setSidebarVisible(!sidebarVisible);
+    }
+
+    return (
+        <div className='main-container'>
+            <button className='toggle-button' onClick={toggleSidebar}>
+                {sidebarVisible ? 'Hide Sidebar' : 'Show Sidebar'}
+            </button>
+            {sidebarVisible && <Sidebar addNewObject={AddNewObg} />}
+            <section className='main-content'>
+                {users.map((item, index) => 
+                    <Card 
+                        key={index}
+                        title={item.title}
+                        avatar={item.avatar}
+                        despretion={item.despretion}
+                        name={item.name}
+                    />
+                )}
+            </section>
+        </div>
+    );
 }
 
-export default Main
+export default Main;
